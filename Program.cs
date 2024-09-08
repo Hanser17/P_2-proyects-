cc2dcbc3239c3dad53266c80cb1263b46eaf2671
Hanser17 <Hanser.pimentel@gmail.com>
2024-09-08 01:07:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_2do
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Hanser ");
+            Console.ReadLine();
+        }
+    }
+}
