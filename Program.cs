0a94f608fe4b6b4a7f2515463f69170fba64acbd
Hanser17 <Hanser.pimentel@gmail.com>
2024-09-08 02:28:38 -0400

    Práctica OOP Herencia

--- Program.cs
+++ Program.cs
@@ -8,10 +8,22 @@ namespace P_2do
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hola pimentel  ");
+            Shape[] shapes = new Shape[]
+            {
+                new Rectangle(alto:2,ancho:4),
+                new Triangle(alto: 7, ancho: 4),
+                new Circle(radio: 10)
+
+            };
+
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine($"El area de la forma {shape.GetType().Name} es: {shape.CalculateShape() }");
+            }
             Console.ReadLine();
+
         }
     }
 }
---
+++ Shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_2do
+{
+   public abstract class Shape
+    {
+        public double Alto {  get; set; }
+        public double Ancho { get; set; }
+        public abstract double CalculateShape();
+  
+      
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(double alto, double ancho) 
+        {
+            Alto= alto;
+            Ancho= ancho;
+        }
+        public override double CalculateShape()
+        {
+            return (Alto * Ancho) / 2;
+           
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle (double alto, double ancho)
+        {
+            Alto = alto;
+            Ancho = ancho;
+        }
+        public override double CalculateShape()
+        {
+            return Alto * Ancho;
+
+        }
+      
+    }
+
+    public class Circle : Shape
+    {
+        public Circle (double radio)
+        {
+            Alto = radio;
+            Ancho = radio;
+        }
+        public override double CalculateShape()
+        {
+            return Math.PI * Math.Pow(Ancho,2);
+        }
+    }
+}
